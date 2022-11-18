98d7443ad1befaa25e414512c047a71787e99b88
Shubham Maurya <65014926+Shubham996633@users.noreply.github.com>
2022-11-18 22:27:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ import Input from './components/Input';
 import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
+import getFormattedWeatherData from './components/services/WeatherServices';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+
+  const [query, setQuery] = useState({ q: 'berlin' })
+  const [units, setUnits] = useState('metric')
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const data = await getFormattedWeatherData
+      console.log(data)
+    }
+    fetchWeather()
+
+  }, [query, units])
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButton />
---
+++ src/components/services/api__key.js
@@ -0,0 +1,3 @@
+const API_KEY = '74ec522cb933c995f0624f8e89b34cbf'
+
+export default API_KEY
